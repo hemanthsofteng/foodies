f9c5e331db82a1965ce53214fd766c21ae6c89e5
hemanthsofteng <hemanadha.softeng@gmail.com>
2024-02-20 21:22:54 +0530

    Update SimpleController.java

--- src/main/java/com/tcp/controller/SimpleController.java
+++ src/main/java/com/tcp/controller/SimpleController.java
@@ -5,12 +5,16 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tcp.model.DisRates;
 import com.tcp.service.SimpleService;
 
-@RestController("/data")
+@RestController
+	@RequestMapping("/data")
+	
+
 public class SimpleController {
 	
 	@Autowired
